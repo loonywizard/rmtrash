549a31f47daf02518e5aedb4812aab11b35eac6b
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2017-11-13 11:44:55 +0300

    Read filename to remove from args

--- rmtrash.sh
+++ rmtrash.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# We serve all files in /home/<username>/.trash directory
 trashDirectoryPath=$HOME/.trash
+
+# We need to delete file in directory, where script was called
+# User passes filename to delete as first argument
+fileToDeletePath=$(pwd)/$1
 
 # if .trash directory doesn't exists
 # -d file checks if file exists and it is a directory 
