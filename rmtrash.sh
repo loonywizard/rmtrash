90abd56ebdf702c17e973dc07f6407450b2390cf
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2017-11-13 12:07:18 +0300

    Generate Id (natural number) for file to remove

--- rmtrash.sh
+++ rmtrash.sh
@@ -3,6 +3,12 @@
 # We serve all files in /home/<username>/.trash directory
 trashDirectoryPath=$HOME/.trash
 
+# We need to use unique identifier for removed file,
+# It is a natural number, and we serve last used natural number
+# in /home/<username>/.trash/.lastUsedId file
+lastUsedIdPath=$trashDirectoryPath/.lastUsedId
+LAST_USED_ID=0
+
 # We need to delete file in directory, where script was called
 # User passes filename to delete as first argument
 fileToDeletePath=$(pwd)/$1
@@ -12,3 +18,11 @@ fileToDeletePath=$(pwd)/$1
 if [ ! -d "$trashDirectoryPath" ]; then
   mkdir "$trashDirectoryPath"
 fi
+
+# -e file checks if file exists
+if [ -e "$lastUsedIdPath" ]; then
+  LAST_USED_ID=$(cat $lastUsedIdPath)
+fi
+
+# Id of current file to remove
+let CURRENT_FILE_ID=LAST_USED_ID+1
